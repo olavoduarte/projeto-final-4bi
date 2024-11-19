08b703b4ae2720a73ae88a720f09c74b54e78d58
Olavo Duarte <169064909+olavoduarte@users.noreply.github.com>
2024-11-18 21:27:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é o melhor jogador de todos os tempos?',
+    'O Cristiano Ronaldo'
+)
+
+criaCartao(
+    'Família',
+    'Quem é meu primo favorito?',
+    'O Kauezin pitiquinho'
+)
+
+criaCartao(
+    'Educação',
+    'Quem é a professora mais legal do planeta?',
+    'A Laís'
+)
+
+criaCartao(
+    'Língua árabe',
+    'لا أستطيع تحمل هذه القمامة المدرسية بعد الآن',
+    'أخيرا انتهت هذه المشاريع'
+)
